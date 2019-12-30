d1a073f0f1c855b9df97402efaa504e4713b7bd8
Chandima Anuradha <canuradhah@gmail.com>
2019-12-30 15:24:11 +0530

    Session 4 Minor Corrections

--- Session4/Group/Main.java
+++ Session4/Group/Main.java
@@ -1,12 +1,20 @@
-import java.lang.String;
-
 public class Main{
   
+   public void validateRegNo(String regNo) {
+   	if(regNo.length()==8 && (regNo.charAt(0)=='S' || regNo.charAt(0)=='s') && (regNo.charAt(1)=='/') && (regNo.charAt(4)=='/') && Character.isDigit(regNo.charAt(2)) && Character.isDigit(regNo.charAt(3)) && Character.isDigit(regNo.charAt(5)) && Character.isDigit(regNo.charAt(6)) && Character.isDigit(regNo.charAt(7))){
+   		System.out.println("Valid RegNo");
+   	}
+   	else {
+   		System.out.println("Invalid RegNo");
+   	}
+   }
+    
     public static void main(String[] args)
     {
       
         final String accessLevel= AccessLevel.FULL_ACCESS.symbol;
         AccessLevel.checkAccess(accessLevel);
+        //AccessLevel.checkAccess(s);
         Personnel p1 = new Personnel();
         
         if (p1.validateLogin())
